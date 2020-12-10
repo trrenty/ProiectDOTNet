b9d2a6b74700a618b69213d4948a51b410f6690e
croftlis <59141582+Elisa-byte@users.noreply.github.com>
2020-12-10 23:54:41 +0200

    ocelot gateway
    In vsc run la ExpressionParser, run la GatewayApi, in browser tab1: http://localhost:7001/api/expressions si in https://localhost:7000/parser-api/expressions => ar trebui sa afiseze acelasi lucru.

---
+++ DOTNET/ExpressionParser/Data/Expression.cs
@@ -0,0 +1,18 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ExpressionParser.Data
+{
+    public class Expression
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("expresie")]
+        public string Expresie { get; set; }
+        [BsonElement("result")]
+        public string Result { get; set; }
+    }
+}
